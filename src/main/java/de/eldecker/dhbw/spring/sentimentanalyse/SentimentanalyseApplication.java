2389b6e6fddc3ee56d86176f25182df1f4a27410
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-06-15 18:24:31 +0200

    Projekt direkt nach Erzeugung
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=4.1.0&packaging=jar&configurationFileFormat=properties&jvmVersion=21&groupId=de.eldecker.dhbw.spring&artifactId=sentimentanalyse&packageName=de.eldecker.dhbw.spring.sentimentanalyse&dependencies=web

---
+++ src/main/java/de/eldecker/dhbw/spring/sentimentanalyse/SentimentanalyseApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.sentimentanalyse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentanalyseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentanalyseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/sentimentanalyse/SentimentanalyseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.sentimentanalyse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentanalyseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
